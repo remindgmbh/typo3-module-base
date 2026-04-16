c9e77d91aad7bf27bcacf76e8c9151c20e1bc669
Lea Kleinrensing <l.kleinrensing@remind.de>
2026-04-16 11:24:31 +0200

    feat!: add v13 compatibility

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,8 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 defined('TYPO3') || die;
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+ExtensionManagementUtility::addStaticFile(
     'rmnd_basemodules',
     'Configuration/TypoScript',
     'REMIND - Base Modules'
